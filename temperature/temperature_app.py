c235ffba1edb63a6d2f3a2ebed44e44bfdb40fed
cristobal_javier@hotmail.com <cristobal_javier@hotmail.com>
2021-12-28 23:42:29 -0300

    Starting temperature/temperature_app.py

---
+++ temperature/temperature_app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+
+
+def main():
+	html_temp = """
+	<h1 style="text-align:center;">Título</h1>
+	"""
+	
+	st.markdown(html_temp, unsafe_allow_html=True)
+	
+	city = st.text_input("Ciudad")
+	country = st.text_input("País")
+	
+	if st.button("Obtener"):
+		cities_df = pd.read_csv("worldcities.csv")[["city", "country", "lat", "lng"]]
+		cities_df["city"] = cities_df["city"].apply(lambda x: x.lower())
+		cities_df["country"] = cities_df["country"].apply(lambda x: x.lower())
+		cities_df2 = cities_df[cities_df["country"] == country]
+		if cities_df2.shape[0] == 0:
+			st.success(f"{city.capitalize()}, {country.capitalize()} "
+					   f"no se encuentra en la base de datos.")
+		else:
+			try:
+				coordinates = cities_df2[cities_df2["city"] == city][[
+					"city", "country", "lat", "lng"
+				]]
+				st.write(f"{city.capitalize()}, {country.capitalize()} "
+						 f"Latitud: {coordinates.lat.values[0]}")
+				st.write(f"{city.capitalize()}, {country.capitalize()} "
+						 f"Longitud: {coordinates.lng.values[0]}")
+			except:
+				st.success(f"{city.capitalize()}, {country.capitalize()} "
+						   f"no se encuentra en la base de datos.")
+
+
+if __name__ == "__main__":
+	main()
