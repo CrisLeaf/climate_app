a9778e978a61087f766d2c6048415cb74833c68a
cristobal_javier@hotmail.com <cristobal_javier@hotmail.com>
2021-12-29 16:03:27 -0300

    Starting streamlit application

--- temperature/temperature_app.py
+++ temperature/temperature_app.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import pandas as pd
+import streamlit.components.v1 as components
 
 
 def main():
 	html_temp = """
-	<h1 style="text-align:center;">Título</h1>
+	<h1 style="text-align: center;">Título</h1>
 	"""
-	
+
+	go_back_link = "[< Go Back](https://crisleaf.github.io/simulations.html)"
+	st.markdown(go_back_link, unsafe_allow_html=True)
+
 	st.markdown(html_temp, unsafe_allow_html=True)
-	
+
 	city = st.text_input("Ciudad")
 	country = st.text_input("País")
-	
+
 	if st.button("Obtener"):
 		cities_df = pd.read_csv("worldcities.csv")[["city", "country", "lat", "lng"]]
 		cities_df["city"] = cities_df["city"].apply(lambda x: x.lower())
