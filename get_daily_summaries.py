508cbcdd94548e1be9a0f6dd2f5c4db262702cfb
cristobal_javier@hotmail.com <cristobal_javier@hotmail.com>
2021-12-29 18:44:40 -0300

    Updated functions inside get_daily_summaries, separated into download_data, data_exists and insert_into_psql

--- get_daily_summaries.py
+++ get_daily_summaries.py
@@ -7,7 +7,7 @@ import psycopg2
 from psql_config import psql_params
 
 
-def get_data(city, country):
+def download_data(city, country):
 	cities_df = pd.read_csv("worldcities.csv")[["city", "country", "lat", "lng"]]
 	cities_df["city"] = cities_df["city"].apply(lambda x: x.lower())
 	cities_df["country"] = cities_df["country"].apply(lambda x: x.lower())
@@ -22,7 +22,6 @@ def get_data(city, country):
 		query_url = "https://www.ncei.noaa.gov/access/search/data-search/daily-summaries?bbox=" + \
 					str(north) + "," + str(west) + "," + str(south) + "," + str(east)
 	except:
-		print(f"({city}, {country}) does not exists!")
 		return
 	
 	browser = webdriver.Firefox()
@@ -45,80 +44,83 @@ def get_data(city, country):
 	data_splited = data.split("\n")
 
 	return data_splited
-	
-def clean_float(value):
-	value = value.replace('"', '')
-	if value == '':
-		return 0.0
-	else:
-		return float(value)
 
-def insert_data_into_psql(city, country):
+def data_exists(city, country):
 	conn = psycopg2.connect(**psql_params)
 	curr = conn.cursor()
-	curr.execute(
-		"SELECT * FROM cities WHERE city = %s AND country = %s", (city, country)
-	)
+	curr.execute("SELECT * FROM cities WHERE city = %s AND country = %s", (city, country))
 	result = curr.fetchall()
+	curr.close()
+	conn.close()
 	
 	if len(result) == 0:
-		curr.execute(
-			"INSERT INTO cities (city, country) VALUES (%s, %s)", (city, country)
-		)
-		curr.execute(
-			"SELECT city_id FROM cities WHERE city = %s AND country = %s", (city, country)
-		)
-		city_id = curr.fetchall()[0][0]
-		print(f"{(city, country)} agregado a la base de datos.")
-		print("Descargando datos...")
-		data = get_data(city, country)
-		if data == None:
-			return
-		for i in range(1, len(data)):
-			row = data[i]. split(",")
-			if len(row) == 4:
+		return False
+	else:
+		return True
+
+def insert_into_psql(city, country, data):
+	conn = psycopg2.connect(**psql_params)
+	curr = conn.cursor()
+	
+	try:
+		curr.execute("INSERT INTO cities (city, country) VALUES (%s, %s)", (city, country))
+	except:
+		conn.rollback()
+	
+	curr.execute("SELECT city_id FROM cities WHERE city = %s AND country = %s", (city, country))
+	city_id = curr.fetchall()[0][0]
+	
+	def clean_float(value):
+		value = value.replace('"', '')
+		if value == '':
+			return 0.0
+		else:
+			return float(value)
+	
+	for i in range(1, len(data)):
+		row = data[i]. split(",")
+		if len(row) == 4:
+			try:
 				curr.execute(
 					"""INSERT INTO temperatures (city_id, station, date, tmax, tmin)
 					VALUES (%s, %s, %s, %s, %s)""",
 					(city_id, row[0], row[1], clean_float(row[2]), clean_float(row[3]))
 				)
-		print("Datos agregados satisfactoriamente!")
-	else:
-		print(f"{(city, country)} ya existe en la base de datos.")
-		curr.execute(
-			"SELECT city_id FROM cities WHERE city = %s AND country = %s", (city, country)
-		)
-		city_id = curr.fetchall()[0][0]
-		while True:
-			redownload = input("Quieres descargar los datos nuevamente?[y/n]:").lower()
-			if redownload == "y":
-				print("Descargando datos...")
-				data = get_data(city, country)
-				if data == None:
-					return
-				for i in range(1, len(data)):
-					row = data[i].split(",")
-					if len(row) == 4:
-						try:
-							curr.execute(
-								"""INSERT INTO temperatures (city_id, station, date, tmax, tmin)
-								VALUES (%s, %s, %s, %s, %s)""",
-								(city_id, row[0], row[1], clean_float(row[2]), clean_float(row[3]))
-							)
-						except:
-							pass
-				print("Datos agregados satisfactoriamente!")
-				break
-			elif redownload == "n":
-				break
-		
+			except:
+				conn.rollback()
+			
 	conn.commit()
 	curr.close()
 	conn.close()
-
+	
 
 if __name__ == "__main__":
 	city = input("Ingrese la ciudad:").lower()
 	country = input("Ingrese el país:").lower()
-	insert_data_into_psql(city, country)
 	
+	exists = data_exists(city, country)
+	if not exists:
+		print("Descargando datos...")
+		data = download_data(city, country)
+		if data == None:
+			print(f"{city.capitalize()}, {country.capitalize()} no se ha podido encontrar.")
+		else:
+			insert_into_psql(city, country, data)
+			print(f"Datos de {city.capitalize()}, {country.capitalize()} agregados "
+				  f"satisfactoriamente!")
+	else:
+		print(f"{city.capitalize()}, {country.capitalize()} ya existe en la base de datos.")
+		while True:
+			redownload = input("Quieres descargar los datos nuevamente?[y/n]:").lower()
+			if redownload == "y":
+				print("Descargando datos...")
+				data = download_data(city, country)
+				if data == None:
+					print(f"{city.capitalize()}, {country.capitalize()} no se ha podido encontrar.")
+				else:
+					insert_into_psql(city, country, data)
+					print(f"Datos de {city.capitalize()}, {country.capitalize()} agregados "
+						  f"satisfactoriamente!")
+				break
+			elif redownload == "n":
+				break
--- temperature_app.py
+++ temperature_app.py
@@ -1,4 +1,44 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from get_daily_summaries import download_data, data_exists, insert_into_psql
 
+
+header = st.container()
+content = st.container()
+
+with header:
+	html_header = """
+		<head>
+		<link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+		</head>
+		<a href="https://crisleaf.github.io/apps.html">
+			<i class="fas fa-arrow-left"></i>
+		</a>
+		<h2 style="text-align:center;">Simulador de Temperaturas</h2>
+		<style>
+            i {
+                font-size: 30px;
+                color: #222;
+            }
+            i:hover {
+            	color: cornflowerblue;
+            	transition: color 0.3s ease;
+            }
+        </style>
+		"""
+	st.markdown(html_header, unsafe_allow_html=True)
+
+with content:
+	city = st.text_input("Ciudad:", placeholder="Ingrese la ciudad... (ex: New York)")
+	country = st.text_input("País:", placeholder="Ingrese el país... (ex: United States)")
+	
+	if st.button("Obtener"):
+		if data_exists(city, country):
+			st.write("Ya Existe")
+		else:
+			st.write(f"{city.capitalize()}, {country.capitalize()} no se encuentra en la base de "
+					 f"datos.")
+			if st.button("Descargar Datos"):
+				st.write(":smile:")
+		
+		
+		
