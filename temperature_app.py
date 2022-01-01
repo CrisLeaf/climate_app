a2af90d95df3751e4a9eb3b6b984aed5ca8f20af
Cris Torres <cristobal_javier@hotmail.com>
2022-01-01 19:05:00 -0300

    updated predictions part

--- temperature_app.py
+++ temperature_app.py
@@ -9,29 +9,29 @@ from psql_config import psql_params
 import psycopg2
 import SessionState
 import plotly.express as px
-from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.ar_model import AutoReg
 from sklearn.metrics import mean_absolute_error
 
 
 html_header = """
-		<head>
-		<link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-		</head>
-		<a href="https://crisleaf.github.io/apps.html">
-			<i class="fas fa-arrow-left"></i>
-		</a>
-		<h2 style="text-align:center;">Simulador de Temperaturas</h2>
-		<style>
-			i {
-				font-size: 30px;
-				color: #222;
-			}
-			i:hover {
-				color: cornflowerblue;
-				transition: color 0.3s ease;
-			}
-		</style>
-	"""
+	<head>
+	<link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	</head>
+	<a href="https://crisleaf.github.io/apps.html">
+		<i class="fas fa-arrow-left"></i>
+	</a>
+	<h2 style="text-align:center;">Simulador de Temperaturas</h2>
+	<style>
+		i {
+			font-size: 30px;
+			color: #222;
+		}
+		i:hover {
+			color: cornflowerblue;
+			transition: color 0.3s ease;
+		}
+	</style>
+"""
 st.markdown(html_header, unsafe_allow_html=True)
 
 city = st.text_input("Ciudad:", placeholder="Ingrese la ciudad... (ex: New York)").lower()
@@ -93,65 +93,61 @@ def get_graphics(city, country):
 	
 	# Predicciones
 	html_title4 = """<h3 style="text-align:center;">Predicción de Temperatura (
-			SARIMAX)</h3>"""
+			Moving Average)</h3>"""
 	st.markdown(html_title4, unsafe_allow_html=True)
-	st.write("(El entrenamiento del modelo puede tardar unos segundos)")
+	
+	monthly_df["tmax_diff"] = monthly_df["Max. Temperature"].diff(1)
+	monthly_df["tmax_lag"] = monthly_df["Max. Temperature"].shift(1)
+	monthly_df["tmin_diff"] = monthly_df["Min. Temperature"].diff(1)
+	monthly_df["tmin_lag"] = monthly_df["Min. Temperature"].shift(1)
+	monthly_df.dropna(inplace=True)
 	
 	## tmax
-	tmax_model = SARIMAX(monthly_df["Max. Temperature"], order=(1, 2, 10),
-						 seasonal_order=(1, 1, 1, 12))
-	tmax_model_fit = tmax_model.fit(maxiter=sarimax_iterations, disp=0)
+	tmax_model = AutoReg(monthly_df["tmax_diff"], lags=12)
+	tmax_model_fit = tmax_model.fit()
 	tmax_residuals = tmax_model_fit.resid
 	tmax_res_fig = px.scatter(tmax_residuals,
 							  labels={"value": "Temperatura (°K)", "Date": "Año"})
 	st.write("Residuales del entreno del modelo de temperatura máxima mensual:")
 	st.write(tmax_res_fig)
 	
-	tmax_yhat = tmax_model_fit.predict()
-	tmax_yearly_pred = tmax_yhat.groupby(pd.Grouper(freq="Y")).mean()
-	tmax_mae = mean_absolute_error(monthly_df["Max. Temperature"], tmax_yhat)
-	st.write("Promedio de error absoluto por mes:", round(tmax_mae, 2))
+	tmax_resid = tmax_model_fit.predict().dropna()
+	ma_tmax_predictions = monthly_df["tmax_lag"].iloc[12:] + tmax_resid
+	tmax_mae = mean_absolute_error(monthly_df["Max. Temperature"].iloc[12:], ma_tmax_predictions)
+	st.write(f"Promedio de Error Absoluto Mensual:", tmax_mae)
 	
-	output = tmax_model_fit.forecast(600)
-	errors = pd.Series(index=output.index,
-					   data=np.random.normal(loc=0, scale=tmax_mae, size=600))
-	output = output + errors
-	predictions = pd.concat([tmax_yhat, output], axis=0)
-	tmax_yearly_pred = predictions.groupby(pd.Grouper(freq="Y")).mean()
-	tmax_pred_df = pd.concat([yearly_df["Max. Temperature"], tmax_yearly_pred], axis=1)
-	tmax_pred_df.columns = ["Original", "Predicted"]
+	st.write("Predicción Anual en el conjunto de entrenamiento:")
+	yearly_df = monthly_df.iloc[12:].groupby(pd.Grouper(freq="Y")).mean()
+	ma_tmax_yearly_prediction = ma_tmax_predictions.groupby(pd.Grouper(freq="Y")).mean()
+	tmax_pred_df = pd.concat([yearly_df["Max. Temperature"], ma_tmax_yearly_prediction], axis=1)
+	tmax_pred_df.columns = ["Original", "Predicción"]
 	tmax_pred_fig = px.line(tmax_pred_df,
 							labels={"value": "Temperatura (°K)", "index": "Año"})
 	st.write(tmax_pred_fig)
 	
 	## tmin
-	st.write("(El entrenamiento del modelo puede tardar unos segundos)")
-	tmin_model = SARIMAX(monthly_df["Min. Temperature"], order=(1, 2, 10),
-						 seasonal_order=(1, 1, 1, 12))
-	tmin_model_fit = tmin_model.fit(maxiter=sarimax_iterations, disp=0)
+	tmin_model = AutoReg(monthly_df["tmin_diff"], lags=12)
+	tmin_model_fit = tmin_model.fit()
 	tmin_residuals = tmin_model_fit.resid
 	tmin_res_fig = px.scatter(tmin_residuals,
 							  labels={"value": "Temperatura (°K)", "Date": "Año"})
 	st.write("Residuales del entreno del modelo de temperatura mínima mensual:")
 	st.write(tmin_res_fig)
 	
-	tmin_yhat = tmin_model_fit.predict()
-	tmin_yearly_pred = tmin_yhat.groupby(pd.Grouper(freq="Y")).mean()
-	tmin_mae = mean_absolute_error(monthly_df["Min. Temperature"], tmin_yhat)
-	st.write("Promedio de error absoluto por mes:", round(tmin_mae, 2))
+	tmin_resid = tmin_model_fit.predict().dropna()
+	ma_tmin_predictions = monthly_df["tmin_lag"].iloc[12:] + tmin_resid
+	tmin_mae = mean_absolute_error(monthly_df["Min. Temperature"].iloc[12:], ma_tmin_predictions)
+	st.write(f"Promedio de Error Absoluto Mensual:", tmin_mae)
 	
-	output = tmin_model_fit.forecast(600)
-	errors = pd.Series(index=output.index,
-					   data=np.random.normal(loc=0, scale=tmin_mae, size=600))
-	output = output + errors
-	predictions = pd.concat([tmin_yhat, output], axis=0)
-	tmin_yearly_pred = predictions.groupby(pd.Grouper(freq="Y")).mean()
-	tmin_pred_df = pd.concat([yearly_df["Min. Temperature"], tmin_yearly_pred], axis=1)
-	tmin_pred_df.columns = ["Original", "Predicted"]
+	st.write("Predicción Anual en el conjunto de entrenamiento:")
+	yearly_df = monthly_df.iloc[12:].groupby(pd.Grouper(freq="Y")).mean()
+	ma_tmin_yearly_prediction = ma_tmin_predictions.groupby(pd.Grouper(freq="Y")).mean()
+	tmin_pred_df = pd.concat([yearly_df["Min. Temperature"], ma_tmin_yearly_prediction], axis=1)
+	tmin_pred_df.columns = ["Original", "Predicción"]
 	tmin_pred_fig = px.line(tmin_pred_df,
 							labels={"value": "Temperatura (°K)", "index": "Año"})
 	st.write(tmin_pred_fig)
-
+	
 obtener_btn = st.empty()
 ss = SessionState.get(obtener_btn=False)
 
