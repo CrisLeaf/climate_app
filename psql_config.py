0456681a91e73365c8884ec466f56458c087a6fa
Cris Torres <cristobal_javier@hotmail.com>
2022-01-07 20:34:40 -0300

    updated psql_config

--- psql_config.py
+++ psql_config.py
@@ -1,18 +1,10 @@
 import os
-import streamlit as st
 
-# psql_params = {
-# 	"host": os.environ["CLIMATE_DATABASE_HOST"],
-# 	"port": os.environ["CLIMATE_DATABASE_PORT"],
-# 	"user": os.environ["CLIMATE_DATABASE_USER"],
-# 	"password": os.environ["CLIMATE_DATABASE_PASSWORD"],
-# 	"database": os.environ["CLIMATE_DATABASE"]
-# }
 
 psql_params = {
-	"host": st.secrets["db_host"],
-	"port": st.secrets["db_port"],
-	"user": st.secrets["db_user"],
-	"password": st.secrets["db_pass"],
-	"database": st.secrets["db_name"],
+	"host": os.environ["db_host"],
+	"port": os.environ["db_port"],
+	"user": os.environ["db_user"],
+	"password": os.environ["db_pass"],
+	"database": os.environ["db_name"]
 }
