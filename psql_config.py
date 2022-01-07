35f3f17cc91ebd2b818e9a469c5464854502b534
Cris Torres <cristobal_javier@hotmail.com>
2022-01-07 20:38:37 -0300

    updated psql_config

--- psql_config.py
+++ psql_config.py
@@ -1,10 +1,18 @@
 import os
+import streamlit as st
 
+# psql_params = {
+# 	"host": os.environ["db_host"],
+# 	"port": os.environ["db_port"],
+# 	"user": os.environ["db_user"],
+# 	"password": os.environ["db_pass"],
+# 	"database": os.environ["db_name"]
+# }
 
 psql_params = {
-	"host": os.environ["db_host"],
-	"port": os.environ["db_port"],
-	"user": os.environ["db_user"],
-	"password": os.environ["db_pass"],
-	"database": os.environ["db_name"]
+	"host": st.secrets["db_host"],
+	"port": st.secrets["db_port"],
+	"user": st.secrets["db_user"],
+	"password": st.secrets["db_pass"],
+	"database": st.secrets["db_name"]
 }
